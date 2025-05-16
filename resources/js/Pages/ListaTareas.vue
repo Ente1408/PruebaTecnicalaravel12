9b164ec351557783f16349f68f822253f96764ae
Ente1408 <entendedor1408@gmail.com>
2025-05-16 16:38:25 -0500

    agrego la vista de la lista con  la referencia de la tabla de tareas
    Ademas agrego la tabla de tareas, donde se vera reflejada la informacion

---
+++ resources/js/Pages/ListaTareas.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import tablaTareas from '@/Pages/Tablatareas.vue'
+</script>
+<template>
+    <Head title="Tareas" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Listado de Tareas
+            </h2>
+        </template>
+
+            <tablaTareas />
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Tablatareas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h2 class="text-xl font-semibold mb-4">Lista de Tareas</h2>
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Descripción</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr v-for="tarea in tareas" :key="tarea.id">
+                                <td class="px-6 py-4 whitespace-nowrap">{{ tarea.id }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ tarea.Descripcion }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { usePage } from '@inertiajs/vue3'
+const { props } = usePage()
+const tareas = props.tareas
+
+</script>
